c8ecc17ffce1937b0de37a1d4c4de3825497af31
chideat <chinaxiahaifeng@gmail.com>
2016-11-09 18:56:49 +0800

    add some test

--- phantomjs_test.go
+++ phantomjs_test.go
@@ -49,3 +49,36 @@ func TestNewSession(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestGetAllSessions(t *testing.T) {
+	options := &Options{}
+	phantomJS, err := NewPhantomJS(10000, options)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		err := phantomJS.Quit()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	session, err := phantomJS.NewSession(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		err = session.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	sessions, err := phantomJS.GetAllSessions()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(sessions) != 1 {
+		t.Fatal("get all sessions failed")
+	}
+}
